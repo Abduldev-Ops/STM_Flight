d4640f6f437e1794b9770ecb05ea611f4ec76d00
Abdulrahman Akanbi <akanbiabdul12@gmail.com>
2026-08-13 00:06:59 -0500

    cascaded angle+rate PID loop

--- Core/Src/task_control.c
+++ Core/Src/task_control.c
@@ -15,8 +15,8 @@
 PID_t pid_rollrate;
 PID_t pid_pitchrate;
 PID_t pid_yawrate;
-
-
+PID_t pid_roll_angle;
+PID_t pid_pitch_angle;
 
 void task_control(void *argument)
 {
@@ -25,10 +25,13 @@ void task_control(void *argument)
 	PID_Init(&pid_rollrate, 0.5f, 0.01f, 0.1f, 50.0f, 100.0f);
 	PID_Init(&pid_pitchrate, 0.5f, 0.01f, 0.1f, 50.0f, 100.0f);
 	PID_Init(&pid_yawrate, 0.3f, 0.01f, 0.05f, 50.0f, 100.0f);
+	PID_Init(&pid_roll_angle, 2.0f, 0.0f, 0.0f, 50.0, 200.0);
+	PID_Init(&pid_pitch_angle, 2.0f, 0.0f, 0.0f, 50.0, 200.0);
 
 	const float dt= 0.01f;
 	const float THROTTLE = 50.0f;
 	FlightState_t state;
+
 
 	for (;;){
 		if (osMessageQueueGet(control_state_queue, &state, NULL, osWaitForever) == osOK)
@@ -36,8 +39,11 @@ void task_control(void *argument)
 			float gyro_x_corrected = state.gyro_x - state.bias_x;
 			float gyro_y_corrected = state.gyro_y - state.bias_y;
 
-			float roll_out = PID_Update(&pid_rollrate, 0, gyro_x_corrected, dt);
-			float pitch_out = PID_Update(&pid_pitchrate, 0, gyro_y_corrected, dt);
+			float roll_rate_setpoint = PID_Update(&pid_roll_angle, 0, state.roll, dt);
+			float pitch_rate_setpoint = PID_Update(&pid_pitch_angle, 0, state.pitch, dt);
+
+			float roll_out = PID_Update(&pid_rollrate, roll_rate_setpoint, gyro_x_corrected, dt);
+			float pitch_out = PID_Update(&pid_pitchrate, pitch_rate_setpoint, gyro_y_corrected, dt);
 			float yaw_out = PID_Update(&pid_yawrate, 0, state.gyro_z, dt);
 
 			float m1= THROTTLE + pitch_out - roll_out - yaw_out;
@@ -56,7 +62,7 @@ void task_control(void *argument)
 
 			if (logging_enabled){
 				osMutexAcquire(uart_mutex, osWaitForever);
-				printf("M1:%.0f M2:%.0f M3:%.0f M4:%.0f\r\n", m1, m2, m3, m4);
+				printf("RS: %.1f PS: %.1f M1:%.0f M2:%.0f M3:%.0f M4:%.0f\r\n", roll_rate_setpoint, pitch_rate_setpoint,m1, m2, m3, m4);
 				osMutexRelease(uart_mutex);
 			}
 		}
